21a98ebf0503b34d07316b8f0b220f329d4d3025
zpzim <zpzimmerman@gmail.com>
2022-06-12 00:47:13 -0700

    Actually fix error output.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -6,10 +6,10 @@ SET CMAKE_GENERATOR=Ninja
 SET CMAKE_GENERATOR_PLATFORM=
 SET CMAKE_GENERATOR_TOOLSET=
 SET PYSCAMP_ADD_CMAKE_ARGS=--trace-expand
-SET CUDAToolkit_ROOT=%CUDA_PATH%
-SET CUDACXX=%CUDA_PATH%\bin\nvcc.exe
-SET CUDAHOSTCXX=%VCToolsInstallDir%\bin\Hostx64\x64\cl.exe
+SET "CUDAToolkit_ROOT=%CUDA_PATH%"
+SET "CUDACXX=%CUDA_PATH%\bin\nvcc.exe"
+SET "CUDAHOSTCXX=%VCToolsInstallDir%\bin\Hostx64\x64\cl.exe"
 SET VERBOSE=ON
-del %BUILD_PREFIX%\bin\nvcc.bat
+del "%BUILD_PREFIX%\bin\nvcc.bat"
 SET
-"%PYTHON%" setup.py install & type %SRC_DIR%\build\lib.win-amd64-cpython-%CONDA_PY%\Release\CMakeFiles\CMakeError.log & type %SRC_DIR%\build\lib.win-amd64-cpython-%CONDA_PY%\Release\CMakeFiles\CMakeOutput.log & type %SRC_DIR%\build\lib.win-amd64-cpython-%CONDA_PY%\Release\CMakeFiles\CMakeError.log & type %SRC_DIR%\build\lib.win-amd64-cpython-%CONDA_PY%\Release\CMakeFiles\CMakeOutput.log
+"%PYTHON%" setup.py install & type %SRC_DIR%\build\temp.win-amd64-cpython-%CONDA_PY%\Release\CMakeFiles\CMakeError.log & type %SRC_DIR%\build\temp.win-amd64-cpython-%CONDA_PY%\Release\CMakeFiles\CMakeOutput.log & type %SRC_DIR%\build\lib.win-amd64-cpython-%CONDA_PY%\Release\CMakeFiles\CMakeError.log & type %SRC_DIR%\build\lib.win-amd64-cpython-%CONDA_PY%\Release\CMakeFiles\CMakeOutput.log
