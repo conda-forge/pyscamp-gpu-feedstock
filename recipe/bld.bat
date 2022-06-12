b76a16997e681a112475a153c95e084145d03927
zpzim <zpzimmerman@gmail.com>
2022-06-12 00:33:12 -0700

    Fix output.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -12,4 +12,4 @@ SET CUDAHOSTCXX=%VCToolsInstallDir%\bin\Hostx64\x64\cl.exe
 SET VERBOSE=ON
 del %BUILD_PREFIX%\bin\nvcc.bat
 SET
-"%PYTHON%" setup.py install & type %SRC_DIR%\build\lib.win-amd64-cpython-%CONDA_PY%\Release\CMakeFiles\CMakeError.log & type %SRC_DIR%\build\lib.win-amd64-cpython-%CONDA_PY%\Release\CMakeFiles\CMakeOutput.log
+"%PYTHON%" setup.py install & type %SRC_DIR%\build\lib.win-amd64-cpython-%CONDA_PY%\Release\CMakeFiles\CMakeError.log & type %SRC_DIR%\build\lib.win-amd64-cpython-%CONDA_PY%\Release\CMakeFiles\CMakeOutput.log & type %SRC_DIR%\build\lib.win-amd64-cpython-%CONDA_PY%\Release\CMakeFiles\CMakeError.log & type %SRC_DIR%\build\lib.win-amd64-cpython-%CONDA_PY%\Release\CMakeFiles\CMakeOutput.log
