2952758c1325aae9a383e03eb2edd79436cd629b
zpzim <zpzimmerman@gmail.com>
2022-06-12 10:52:45 -0700

    Cleanup some debug settings.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,13 +5,14 @@ SET PYSCAMP_USE_EXTERNAL_PYBIND11=ON
 ::SET CMAKE_GENERATOR=Ninja
 ::SET CMAKE_GENERATOR_PLATFORM=
 ::SET CMAKE_GENERATOR_TOOLSET=
-SET PYSCAMP_ADD_CMAKE_ARGS=--trace-expand
+::SET PYSCAMP_ADD_CMAKE_ARGS=--trace-expand
 ::SET "CUDAToolkit_ROOT=%CUDA_PATH%"
 ::SET "CUDACXX=%CUDA_PATH%\bin\nvcc.exe"
 ::SET "CUDAHOSTCXX=%VCToolsInstallDir%\bin\Hostx64\x64\cl.exe"
 SET VERBOSE=ON
 ::SET "PATH=%VCToolsInstallDir%\bin\Hostx64\x64;%BUILD_PREFIX%;%BUILD_PREFIX%\Library\usr\bin;%BUILD_PREFIX%\Library\bin;%BUILD_PREFIX%\Scripts;%PREFIX%\Library\usr\bin;;%PREFIX%\Library\bin;%PREFIX%\Scripts;%PREFIX%\Scripts;C:\Miniforge;C:\Miniforge\Library\usr\bin;C:\Miniforge\Library\bin;C:\Miniforge\Scripts;C:\Miniforge\bin;C:\Miniforge\condabin"
 ::del %BUILD_PREFIX%\bin\nvcc.bat
-SET
+::SET
+"%PYTHON%" setup.py install
 
-"%PYTHON%" setup.py install & type %SRC_DIR%\build\temp.win-amd64-cpython-%CONDA_PY%\Release\CMakeFiles\CMakeError.log & type %SRC_DIR%\build\temp.win-amd64-cpython-%CONDA_PY%\Release\CMakeFiles\CMakeOutput.log & type %SRC_DIR%\build\lib.win-amd64-cpython-%CONDA_PY%\Release\CMakeFiles\CMakeError.log & type %SRC_DIR%\build\lib.win-amd64-cpython-%CONDA_PY%\Release\CMakeFiles\CMakeOutput.log
+::"%PYTHON%" setup.py install & type %SRC_DIR%\build\temp.win-amd64-cpython-%CONDA_PY%\Release\CMakeFiles\CMakeError.log & type %SRC_DIR%\build\temp.win-amd64-cpython-%CONDA_PY%\Release\CMakeFiles\CMakeOutput.log & type %SRC_DIR%\build\lib.win-amd64-cpython-%CONDA_PY%\Release\CMakeFiles\CMakeError.log & type %SRC_DIR%\build\lib.win-amd64-cpython-%CONDA_PY%\Release\CMakeFiles\CMakeOutput.log
