3cfad4e5ee828ca9193873e8b36a52df2e55a293
Zach Zimmerman <zpzimmerman@gmail.com>
2026-05-03 21:09:05 -0600

    test: pin SETUPTOOLS_SCM_PRETEND_VERSION for dev build

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,4 +3,6 @@ export SCAMP_ENABLE_BINARY_DISTRIBUTION=ON
 export SCAMP_USE_EXTERNAL_EIGEN=ON
 export PYSCAMP_USE_EXTERNAL_PYBIND11=ON
 export FORCE_CUDA=ON
+# Pin version so setuptools_scm does not produce a local-version string
+export SETUPTOOLS_SCM_PRETEND_VERSION=4.0.2dev0
 ${PYTHON} setup.py install
